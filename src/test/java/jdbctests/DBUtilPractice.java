edfa981553e697222a20c9d35ebd6c7a264dce2a
VadymLysenko <vesioluy@hotmail.com>
2021-08-08 14:54:22 -0500

    added code to DBUtilPractice

--- src/test/java/jdbctests/DBUtilPractice.java
+++ src/test/java/jdbctests/DBUtilPractice.java
@@ -1,6 +1,27 @@
 package jdbctests;
 
+import org.junit.jupiter.api.Test;
+import utilities.DBUtils;
+
+import java.util.List;
+import java.util.Map;
+
 public class DBUtilPractice {
 
+    @Test
+    public void test1(){
+    DBUtils.createConnection();
+    String query = "select first_name,last_name,salary,job_id\n" +
+            "from employees\n" +
+            "where rownum < 6";
+    List<Map<String, Object>> queryData = DBUtils.getQueryResultMap(query);
 
+        for (Map<String,Object> row : queryData){
+            System.out.println(row.toString());
+        }
+
+    DBUtils.destroy();
+
+    }
 }
+
